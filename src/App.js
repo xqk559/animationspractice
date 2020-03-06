25b9b40f3425380fb391557b71ff4046977f24f1
keith <xqk559@gmail.com>
2020-03-06 07:30:13 -0500

    Added react-transition-group and <Transition> wrapper to allow full animations in react conditionally

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import Transition from 'react-transition-group/Transition';
 import "./App.css";
 import Modal from "./components/Modal/Modal";
 import Backdrop from "./components/Backdrop/Backdrop";
@@ -22,6 +22,22 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
+        <button className="Button" onClick={()=> this.setState(prevState => ({showBlock: !prevState.showBlock}))}>Toggle</button>
+        <br />
+        <br />
+        <Transition in={this.state.showBlock} 
+                    timeout={1000}
+                    mountOnEnter
+                    unmountOnExit>
+            {state => (<div style={{
+                            backgroundColor: 'red',
+                            width: 100,
+                            height: 100,
+                            margin: 'auto',
+                            transition: 'opacity 1s ease-out',
+                            opacity: state === 'exiting' ? 0 : 1}}/>)
+            }
+        </Transition>
         {this.state.modalIsOpen ? <Modal show={this.state.modalIsOpen} closed={this.closeModal}/> : null}
         {this.state.modalIsOpen ? <Backdrop show={this.state.modalIsOpen} /> : null}
         <button className="Button" onClick={this.showModal}>Open Modal</button>
